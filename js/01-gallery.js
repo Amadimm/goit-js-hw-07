aca9331739ed718c4390fc95304929923b3d78c5
Amadimm <amadeuszrolecki@gmail.com>
2023-05-01 10:42:40 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(`.gallery`);
+
+const newImages = galleryItems.map(
+    (image) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(" ");
+
+galleryList.insertAdjacentHTML("beforeend", newImages);
+
+galleryList.addEventListener("click", (e) => {
+  e.preventDefault();
+ 
+ 
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}  ">`
+  );
+  
+  const close = document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener(Object, close);
+    }
+  });
+
+  instance.show();
+});
