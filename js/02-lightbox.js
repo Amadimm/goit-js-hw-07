06a4cfa52bdd1090114a181444bedfc272b2b710
Amadimm <amadeuszrolecki@gmail.com>
2023-05-01 11:40:31 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(`.gallery`);
+
+const newImages = galleryItems.map(
+    (image) =>
+      `<li>
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+   
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(" ");
+
+galleryList.insertAdjacentHTML("beforeend", newImages);
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
